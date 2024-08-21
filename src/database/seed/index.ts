7a6664ff4aca6dc96639a7aa936652469b42f99c
Joshua Jack <joshua.jack@gmail.com>
2024-08-20 21:27:09 -0400

    refactor(config): 🔧 update Payload config import syntax
    To support new version compatibility

--- src/database/seed/index.ts
+++ src/database/seed/index.ts
@@ -1,5 +1,5 @@
 import { getPayload } from 'payload'
-import { importConfig } from 'payload/node'
+import config from '@payload-config'
 import addRoles from './Roles'
 import addUsers from './Users'
 
@@ -7,7 +7,6 @@ import addUsers from './Users'
  * Seeds the database, in order
  */
 async function seed(): Promise<void> {
-  const config = await importConfig('../../payload.config.ts')
   const payload = await getPayload({ config })
 
   await addRoles(payload)
